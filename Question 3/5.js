9658e1ea299b4affee80a8287b8937df450f3059
Sushant Patil <patilsushant518@gmail.com>
2023-01-12 23:59:28 +0530

    discription added

--- Question 3/5.js
+++ Question 3/5.js
@@ -11,3 +11,6 @@ function check()
 }
 
 console.log(check());
+
+//.  No , If that loop is not in the function we can't use return but if that loop is in the function then we use the return which stops the execution at that position and start from where the function 
+//is called.
